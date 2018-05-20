6a37bdc301f56ccd7a278de37c97bacccc4b66c9
cklasicki <cklasicki@gmail.com>
2018-05-20 23:35:19 +0200

    dodanie metody tearDown

--- TestCaseTest.py
+++ TestCaseTest.py
@@ -9,33 +9,34 @@ class TestCase(object):
         self.setUp()
         method = getattr(self, self.name)
         method()
+        self.tearDown()
+
+    def tearDown(self):
+        pass
 
 
 class WasRun(TestCase):
     def __init__(self, name):
-        # tego nie moge przeniesc do metody setup
-        self.wasRun = None
         TestCase.__init__(self, name)
 
-    def testMethod(self):
-        self.wasRun = 1
-
     def setUp(self):
-        self.wasSetUp = 1
+        self.log = "setUp "
+
+    def testMethod(self):
+        self.log = self.log + "testMethod "
+
+    def tearDown(self):
+        self.log = self.log + "tearDown "
 
 
 class TestCaseTest(TestCase):
     def setUp(self):
-        self.test = WasRun("testMethod")
+        pass
 
-    def testRunning(self):
-        self.test.run()
-        assert(self.test.wasRun)
-
-    def testSetUp(self):
-        self.test.run()
-        assert(self.test.wasSetUp)
+    def testTemplateMethod(self):
+        test = WasRun("testMethod")
+        test.run()
+        assert("setUp testMethod tearDown " == test.log)
 
 
-TestCaseTest("testRunning").run()
-TestCaseTest("testSetUp").run()
+TestCaseTest("testTemplateMethod").run()
